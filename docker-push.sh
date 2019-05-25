95b6b768e1486537cfe3c25cefa797b8a81676f6
Luke Thompson <endeavour9@gmail.com>
2019-05-25 14:14:07 +1000

    add docker script and docker file

---
+++ docker-push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+aws cloudformation describe-stacks --stack-name SydneyJavaContainers > cfn-output.json
+ECR_REPO=$(jq < cfn-output.json -r '.Stacks[0].Outputs[] | select(.OutputKey=="'EcrRepo'") | .OutputValue')
+
+$(aws ecr get-login --no-include-email)
+
+docker build -t sydney-java-service .
+docker tag sydney-java-service:latest $ECR_REPO:latest
+# docker push $ECR_REPO:latest
