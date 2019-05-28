a419b22e72da96f8f9fb033a76e91a30e226508e
Luke Thompson <endeavour9@gmail.com>
2019-05-28 18:11:57 +1000

    remove old mysfits steps

--- k6.js
+++ k6.js
@@ -1,10 +1,12 @@
 import { check } from "k6";
 import http from "k6/http";
 
-export default function() {
-  http.post("http://localhost:8080/", JSON.stringify({id: "4321", url: "http://k6.test", popped: true}), { headers: {"Content-Type": "application/json"} })
+let host = "alb-SydneyJavaContainers-873543138.ap-southeast-2.elb.amazonaws.com" //"localhost:8080"
 
-  let res = http.get("http://localhost:8080/4321/");
+export default function() {
+  http.post("http://" + host + "/", JSON.stringify({id: "4321", url: "http://k6.test", popped: true}), { headers: {"Content-Type": "application/json"} })
+
+  let res = http.get("http://" + host + "/4321/");
   check(res, {
           "is status 200": (r) => r.status === 200,
           "returned correct balloon id": (r) => r.json("id") === "4321",
